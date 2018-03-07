0938bca5af7c1f6052214dfc172a6c4f60359ae5
leizvur <35609834+leizvur@users.noreply.github.com>
2018-03-08 00:10:46 +0300

    Решены проблемы с кодировкой, простейшая верстка

--- books.php
+++ books.php
@@ -1,48 +1,46 @@
 <?php
 
-/*define("DB_DRIVER", "mysql");
-define("DB_HOST", "localhost");
-define("DB_NAME", "value");
-define("DB_USER", "rodionova");
-define("DB_PASS", "neto1663");
-
-try{
-	//подключаемся к базе данных
-	$connect_str=DB_DRIVER . ";host=" . DB_HOST . ";dbname=" . DB_NAME;
-	$db=new PDO($connect_str,DB_USER, DB_PASS); 
-	$row=[];
-	//делаем запрос к таблице базы данных
-    $result=$db->query("SELECT * FROM books");
-	$error_array=$db->errorInfo();
-	
-	//обрабатываем ошибки
-	if ($db->errorCode()!=0000) 
-	{
-		echo "SQL error: " . $error_array[2] . "<br /><br />";
-	} 
-
-	while ($row= $result->fetch()) 
-	{
-		print_r($row);
-	}
-}*/
-
 $connect=mysqli_connect("localhost", "rodionova", "neto1663", "global");
+$connect->set_charset("utf8");
 $sql="select * from books";
 $res=mysqli_query($connect, $sql);
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Библиотека успешного человека<</title>
+	<style type="text/css">
+		table {
+    		border-collapse: collapse;
+    		border: 1px solid #ccc;
+   		}
+   		tr, td, th {
+    		padding: 5px;
+   			border: 1px solid #ccc;
+   		}
+   		th {
+    		background-color: #EFEEEC; 
+    		font-weight: bold; 
+    		text-align: center;
+   		}
+	</style>
+</head>
+<body>
 
-while ($data=mysqli_fetch_array($res))
-{?>
+</body>
+</html>
+<h1>Библиотека успешного человека</h1>
 <table>
 	<thead>
-		<tr>
-			<td>Название</td>
-			<td>Автор</td>
-			<td>Год выпуска</td>
-			<td>Жанр</td>
-			<td>ISBN</td>
-		</tr>
+		<th>Название</th>
+		<th>Автор</th>
+		<th>Год выпуска</th>
+		<th>Жанр</th>
+		<th>ISBN</th>
 	</thead>
+	    <?php
+		while ($data=mysqli_fetch_array($res))
+		{?>
 	<tbody>
 		<tr>
 			<td><?php echo $data["name"] ?></td>
@@ -52,8 +50,8 @@ while ($data=mysqli_fetch_array($res))
 			<td><?php echo $data["isbn"] ?></td>
 		</tr>
 	</tbody>
-</table>
-<?php
-}
+		<?php
+		}
 
-?>
+			?>
+</table>
